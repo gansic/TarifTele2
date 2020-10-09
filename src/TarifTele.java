74d7c761576c1fe07453fe65b26c7c23f2c81f98
gansic <rus.com.rf@gmail.com>
2020-10-09 21:57:54 +0300

    initial commit

---
+++ src/TarifTele.java
@@ -0,0 +1,16 @@
+public class TarifTele {
+    long id;
+    String titlename; // название тарифа
+    double time; //
+    String tarifname; //  тариф
+    int trafic;
+    String withoutlimit;
+    int cost;
+    String month;
+
+
+
+
+
+
+}
